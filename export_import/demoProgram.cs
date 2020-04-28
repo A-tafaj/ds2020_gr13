e81dd5e6ed082e62d414d0ad5327df520db7cbfd
Ali Tafaj <58335577+A-tafaj@users.noreply.github.com>
2020-04-28 19:05:18 +0200

    Add files via upload

---
+++ export_import/demoProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Transfer
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            if (args.Length > 0)
+            {
+                if (args[0] == "export-key")
+                {
+                    string emri = args[1];
+                }
+                else if (args[0] == "import-key")
+                {
+                    string emri = args[1];
+                }
+                else
+                {
+                    Console.WriteLine("Shtypni njeren nga keto komanda: <export-key> ose <import-key>");
+                }
+
+
+            }
+
+
+        }
+    }
+
+}
