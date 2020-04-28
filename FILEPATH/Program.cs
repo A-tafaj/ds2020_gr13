467a5c6894edf5b28a528478f2d4577a0d07cfa0
Banti <bantlushaj@gmail.com>
2020-04-28 07:07:21 +0200

    write-msg:read-msg (priv)

---
+++ FILEPATH/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+namespace RS
+{
+    class Test
+    {
+        public static void Main(String[] args)
+        {
+            if (args.Length > 0)
+            {
+                if (args[0] == "write-message")
+                {
+                    string input = args[1];
+                    string pubkey = "keys/" + input + ".pub.xml";
+                    DESCryptoServiceProvider objDes = new DESCryptoServiceProvider();
+                    objDes.GenerateKey();
+                    objDes.GenerateIV();
+                    var randKey = Convert.ToBase64String(objDes.Key);
+                    var randIV = Convert.ToBase64String(objDes.IV);
+
+                    if (File.Exists(pubkey))
+                    {
+                        string publicKey = File.ReadAllText("keys/" + input + ".pub.xml");
+                        string tekst = args[2];
+                       // Console.WriteLine(RSA.Encrypt(tekst, publicKey)+"\n\n");
+                      //  Console.WriteLine(publicKey);
+                        Console.WriteLine("\n"+Base64Encode(input) + "." + randIV+"."+ RSA.Encrypt(tekst, publicKey));
+
+                    }
+                }
+
+            }
+            static string Base64Encode(string plainText)
+            {
+                var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+                return Convert.ToBase64String(plainTextBytes);
+            }
+
+
+
+
+
+        }
+    }
+
+}
---
+++ FILEPATH/rsa.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RS
+{
+    class RSA
+    {
+       
+        public static string Encrypt(string textToEncrypt, string publicKeyString)
+        {
+            var bytesToEncrypt = Encoding.UTF8.GetBytes(textToEncrypt);
+
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                try
+                {
+                    rsa.FromXmlString(publicKeyString.ToString());
+                    var encryptedData = rsa.Encrypt(bytesToEncrypt, true);
+                    var base64Encrypted = Convert.ToBase64String(encryptedData);
+                    return base64Encrypted;
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+        public static string Decrypt(string textToDecrypt, string privateKeyString)
+        {
+            var bytesToDescrypt = Encoding.UTF8.GetBytes(textToDecrypt);
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                try
+                {
+
+                    rsa.FromXmlString(privateKeyString);
+
+                    var resultBytes = Convert.FromBase64String(textToDecrypt);
+                    var decryptedBytes = rsa.Decrypt(resultBytes, true);
+                    var decryptedData = Encoding.UTF8.GetString(decryptedBytes);
+                    return decryptedData.ToString();
+                }
+                finally
+                {
+                    rsa.PersistKeyInCsp = false;
+                }
+            }
+        }
+
+
+    }
+}
+
+
